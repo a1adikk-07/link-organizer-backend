f61c7bbc8d6282fa94e5c029df076474338d3641
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-07-01 18:23:19 +0300

    commit

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-import yargs from "yargs";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-import * as linkCardsService from "./links/index.js";
+import contactsRouter from "./routes/contactsRouter.js";
 
-const invokeAction = async ({ action, id, ...data }) => {
-  switch (action) {
-    case "list":
-      const AllLinks = await linkCardsService.getAllLinks();
-      return console.log(AllLinks);
-    case "getById":
-      const linksCard = await linkCardsService.getLinksById(id);
-      return console.log(linksCard);
-    case "add":
-      const newLinkCard = await linkCardsService.addLinkCard(data);
-      return console.log(newLinkCard);
-    case "updateById":
-      const updateCard = await linkCardsService.updateCardById(id, data);
-      return console.log(updateCard);
-    case "deleteById":
-      const deleteCard = await linkCardsService.deleteCardById(id);
-      return console.log(deleteCard);
-    default:
-      console.log("Unknown action");
-  }
-};
+const app = express();
 
-const { argv } = yargs(process.argv.slice(2));
-invokeAction(argv);
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});
